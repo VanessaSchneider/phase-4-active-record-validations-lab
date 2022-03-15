0d32083db4946ad9dfe0bfd190dd7258a0188f3d
Vanessa Schneider <vanessajwade@gmail.com>
2022-03-14 21:04:27 -0400

    commit tree 837af4fe71eea6e6ef9fd3ab31f605bc3e0ebba1

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+        validates :name, presence, uniqueness: true
+        validates :phone_number, length: { is: 10 }
+
+
+def create 
+        author = Author.create(name: params[:name], phone_number: params[phone_number])
+end 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-fiction)
+    validate: validator
+
+
+    def vaidator
+        unless title.match?("Won't Believe" "Secret" "Top [number]" "Guess")
+          errors.add(:email, "Title must include Clickbait")
+        end
+    end
+
+
+
+    def create 
+        post = Post.create(title: params[:title], content: params[:content],summary: params[:summary], category: params[:caegory],)
+
+end
 end
